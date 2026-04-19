54e3ec57e0fcf35b7fd121a12ba76e8f0618c198
mcypher-alt <nikich-varganov@mail.ru>
2026-04-19 17:13:03 +0500

    разворачивание заметок при нажатии на кнопку

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,26 @@ import { useEffect, useState } from 'react'
 
 export default function ToDoApp() {
   const [noteList, setNoteList] = useState([]);
-  const [form, setForm] = useState({title: '', description: ''});
+  const [form, setForm] = useState({ title: '', description: '' });
+  const [noteId, setNoteId] = useState(null);
+
   const renderList = () => {
     if (noteList.length !== 0) {
-      return (
-        noteList.map((note) => {
-          return (
-          <div key={note.id}>
-            <h1>{note.title}</h1>
-            <p>{note.description}</p>
-          </div>
-          );
-      }
-        )
-          )
-            }
-    else return <p>Задача в списке нет. Добавьте первую!</p>
+      return noteList.map((note) => (
+        <div key={note.id}>
+          <button onClick={() => setNoteId(noteId === note.id ? null : note.id)}>
+            {note.title}
+          </button>
+          {noteId === note.id && <p>{note.description}</p>}
+        </div>
+      ));
+    }
+    return <p>Задач в списке нет. Добавьте первую!</p>;
   };
 
   const handleAdd = () => {
+    if (form.title.trim() === '') return;
+
     const newTask = {
       id: Date.now(),
       title: form.title,
@@ -29,36 +30,34 @@ export default function ToDoApp() {
     const updatedList = [...noteList, newTask];
     setNoteList(updatedList);
     localStorage.setItem('notes', JSON.stringify(updatedList));
-    setForm({title: '', description: ''});
+    setForm({ title: '', description: '' });
   };
 
   useEffect(() => {
     const saved = localStorage.getItem('notes');
     if (saved) {
-        setNoteList(JSON.parse(saved));
+      setNoteList(JSON.parse(saved));
     }
-}, []);
+  }, []);
 
   return (
     <div>
       <div>
         <input
-        value={form.title}
-        type='text'
-        placeholder='Введите название'
-        onChange={(e) => setForm({...form, title: e.target.value})}
-        ></input>
+          value={form.title}
+          type="text"
+          placeholder="Введите название"
+          onChange={(e) => setForm({ ...form, title: e.target.value })}
+        />
         <input
-        value={form.description}
-        type='text'
-        placeholder='Введите текст заметки'
-        onChange={(e) => setForm({...form, description: e.target.value})}
-        ></input>
+          value={form.description}
+          type="text"
+          placeholder="Введите текст заметки"
+          onChange={(e) => setForm({ ...form, description: e.target.value })}
+        />
         <button onClick={handleAdd}>Добавить</button>
       </div>
-      <div>
-        {renderList()}
-      </div>
+      <div>{renderList()}</div>
     </div>
-  )
-};
+  );
+}
